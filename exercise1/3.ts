5b2e876b1a198e1049cccdf7f7b09460db451d15
ChaiGans <elbertchailes888@gmail.com>
2024-03-23 16:00:19 +0700

    .

--- exercise1/3.ts
+++ exercise1/3.ts
@@ -3,4 +3,6 @@ let substract = function (a: number, b: number): number {
   return a - b;
 };
 
+let substract1 = substract(1, 2);
+console.log(substract1);
 console.log("Variabel pengurangan dua angka : ", substract(1, 2));
--- exercise1/4.ts
+++ exercise1/4.ts
@@ -5,8 +5,6 @@ let divide = function (a: number, b: number): number {
 
 console.log("Variabel pembagian dua angka : ", divide(1, 2));
 
-let substract1 = substract(1, 2);
-console.log(substract1);
 
 function sub(a: number, b: number): number {
   return a - b;
